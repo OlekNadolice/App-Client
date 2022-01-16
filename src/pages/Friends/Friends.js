6f3f829aaa301c5bd1923c6fb0ba071a8c2ae231
Aleksander Cygan <aleksandercygan@MacBook-Air-Aleksander.local>
2022-01-16 08:11:30 +0100

    refactor: Remove fetch calls from the component and add fetch call with custom hook

--- src/pages/Friends/Friends.js
+++ src/pages/Friends/Friends.js
@@ -2,46 +2,35 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import classes from "./friends.module.css";
 import ClipLoader from "react-spinners/ClipLoader";
-import friends from "./testData";
+import useQuery from "../../hooks/useQuery";
+
 const Friends = () => {
   const { id } = useParams();
-  // const [friends, setFriends] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [message, setMessage] = useState("");
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   fetch(`http://localhost:8000/auth/users?id=${id}&&type=friends`, {
-  //     headers: {
-  //       authorization: `Bearer ${localStorage.getItem("token")}`,
-  //     },
-  //   })
-  //     .then(response => {
-  //       if (response.status === 200) {
-  //         return response.json();
-  //       } else {
-  //         throw new Error("Ups something went wrong");
-  //       }
-  //     })
-  //     .then(data => {
-  //       console.log(data.data.friends);
-  //       setIsLoading(false);
-  //       setFriends(data.data.friends);
-  //       data.data.friends.length === 0 &&
-  //         setMessage("This user doesnt have any friends right now");
-  //     });
-  // }, []);
+  const [friends, setFriends] = useState([]);
+
+  const { data, loading, error } = useQuery(
+    `http://localhost:8000/auth/users?id=${id}&&type=friends`,
+    {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+
+  useEffect(() => {
+    data && setFriends(data.data.friends);
+  }, [data]);
 
   return (
     <div className={classes.container}>
-      {/* <ClipLoader loading={isLoading} size={"120px"} color="#803939" /> */}
-      {/* {message && message} */}
+      <ClipLoader loading={loading} size={"120px"} color="#803939" />
+      {!loading && friends.length === 0 && <p>This user doesn't have any friends.</p>}
       {friends.length > 0 &&
+        !loading &&
         friends.map(e => {
           return (
-            // <div className={classes.friendBox} key={e._id}>
             <div className={classes.friendBox} key={e.name + Math.random()}>
-              {/* <img src={`http://localhost:8000/images/${e.profileImage}`} alt="" /> */}
-              <img src={`${e.profileImage}`} alt="" />
+              <img src={`http://localhost:8000/images/${e.profileImage}`} alt="" />
               <div>
                 <h3>{e.name}</h3>
                 <Link to={`/users/${e._id}`}>Wyswietl Profil </Link>
