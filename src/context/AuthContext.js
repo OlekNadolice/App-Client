54d3a44a3bb71968030918f8d467d2e1c6a897f0
Aleksander Cygan <aleksandercygan@MacBook-Air-Aleksander.local>
2022-01-21 15:06:54 +0100

    Add Socket IO to global state

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, createContext } from "react";
-
+import { io } from "socket.io-client";
 export const authContext = createContext();
 
 const AuthContextProvider = props => {
@@ -7,9 +7,30 @@ const AuthContextProvider = props => {
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("token"));
   const [userImage, setUserImage] = useState(localStorage.getItem("profileImage"));
 
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    setSocket(
+      io("http://127.0.0.1:8000", {
+        query: {
+          user: localStorage.getItem("id"),
+        },
+        autoConnect: false,
+      })
+    );
+  }, [isLoggedIn]);
+
   return (
     <authContext.Provider
-      value={{ isLoggedIn, setIsLoggedIn, token, setToken, userImage, setUserImage }}
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        token,
+        setToken,
+        userImage,
+        setUserImage,
+        socket,
+      }}
     >
       {props.children}
     </authContext.Provider>
