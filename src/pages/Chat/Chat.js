4ab29f27f2c2629c0d037196d9a72a872aa51b57
Aleksander Cygan <aleksandercygan@MacBook-Air-Aleksander.local>
2022-01-23 20:09:01 +0100

    Removed connection with Socket Io in this component and added automatic scroll on every message in the chat

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,39 +1,18 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import classes from "./chat.module.css";
-import useQuery from "../../hooks/useQuery";
-import { io } from "socket.io-client";
-
-// const socket = io("http://127.0.0.1:8000", {
-//   query: {
-//     user: localStorage.getItem("id"),
-//   },
-//   autoConnect: false,
-
-// });
+import useQuery from "hooks/useQuery";
+import { authContext } from "context/AuthContext";
+import ClipLoader from "react-spinners/ClipLoader";
+import { AiOutlineSend } from "react-icons/ai";
 
 const Chat = () => {
   const [users, setUsers] = useState([]);
   const messageInput = useRef("");
+  const messagesEndRef = useRef(null);
+  const [currentUserName, setCurrentUserName] = useState("");
   const [message, setMessage] = useState([]);
   const [currentUser, setCurrentUser] = useState("");
-  const socket = useMemo(
-    () =>
-      io("http://127.0.0.1:8000", {
-        query: {
-          user: localStorage.getItem("id"),
-        },
-        autoConnect: false,
-      }),
-    []
-  );
-
-  useEffect(() => {
-    socket.connect();
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+  const { socket } = useContext(authContext);
 
   const { data, loading, error } = useQuery("http://localhost:8000/messenger/", {
     headers: {
@@ -42,55 +21,68 @@ const Chat = () => {
   });
 
   useEffect(() => {
-    data && setUsers(data.data.friends);
-    data && socket.emit("active", { id: localStorage.getItem("id") });
-    console.log("renderuje sie typie znowu..");
+    let componentMounted = true;
+    componentMounted && (document.title = "Chat");
+    return () => (componentMounted = false);
+  }, []);
+
+  useEffect(() => {
+    messagesEndRef?.current.scroll({ top: messagesEndRef.current.scrollHeight });
+  }, [message]);
+
+  useEffect(() => {
+    let componentMounted = true;
+
+    if (componentMounted && data) {
+      setUsers(data.data.friends);
+      socket.emit("active", { id: localStorage.getItem("id") });
+    }
+
+    return () => (componentMounted = false);
   }, [data]);
 
   useEffect(() => {
-    socket.on("message", message => {
-      if (message.author === currentUser) {
-        setMessage(prevState => [
-          ...prevState,
-          { text: message.message, author: message.author },
-        ]);
-      }
-    });
+    if (socket) {
+      socket.on("message", message => {
+        if (message.author === currentUser) {
+          setMessage(prevState => [
+            ...prevState,
+            {
+              text: message.message,
+              author: message.author,
+            },
+          ]);
+        }
+      });
 
-    socket.on("findChat", data => {
-      console.log("mamy chat");
-      if (!data) {
-        setMessage([]);
-        console.log("nie ma czatu ale dziala");
-      }
-      data &&
-        setMessage(
-          data.map(e => {
-            return { text: e.text, author: e.author };
-          })
-        );
-    });
+      socket.on("findChat", data => {
+        data
+          ? setMessage(
+              data.map(e => {
+                return { text: e.text, author: e.author, created: e.created };
+              })
+            )
+          : setMessage([]);
+      });
 
-    socket.on("active", data => {
-      if (data.length > 0) {
-        const activeUsers = data.map(e => e.id);
-        const updatedUsers = users.map(e => {
-          if (activeUsers.includes(e._id)) {
-            return (e = { ...e, online: true });
-          } else {
-            return (e = { ...e });
-          }
-        });
-        console.log("renderuje ciagle");
-        setUsers([...updatedUsers]);
-      }
-    });
-
-    // return () => socket.removeAllListeners();
+      socket.on("active", response => {
+        if (response.length > 0) {
+          const activeUsers = response.map(e => e.id);
+          const updatedUsers = users.map(e => {
+            if (activeUsers.includes(e._id)) {
+              return (e = { ...e, online: true });
+            } else {
+              return (e = { ...e });
+            }
+          });
+          setUsers([...updatedUsers]);
+        }
+      });
+      return () => socket.removeAllListeners();
+    }
   });
 
   const sentMessage = async () => {
-    console.log(currentUser);
     await socket.emit("message", {
       message: messageInput.current.value,
       id: localStorage.getItem("id"),
@@ -98,20 +90,31 @@ const Chat = () => {
     });
     setMessage(prevState => [
       ...prevState,
-      { text: messageInput.current.value, author: localStorage.getItem("id") },
+      {
+        text: messageInput.current.value,
+        author: localStorage.getItem("id"),
+        created: new Date(Date.now()),
+      },
     ]);
     messageInput.current.value = "";
   };
 
-  const findChatMessages = async secondUserId => {
+  const findChatMessages = async (secondUserId, name) => {
     setCurrentUser(secondUserId);
+    setCurrentUserName(name);
     await socket.emit("findChat", {
       id: localStorage.getItem("id"),
       secondUserId: secondUserId,
     });
-
-    console.log("zmienione user");
   };
+
+  if (loading) {
+    return (
+      <div className={classes.loadingContainer}>
+        <ClipLoader size={"100"} color={"#803939"} loading={loading}></ClipLoader>
+      </div>
+    );
+  }
 
   return (
     <div className={classes.chatContainer}>
@@ -119,7 +122,10 @@ const Chat = () => {
         {users.length > 0 &&
           users.map(element => {
             return (
-              <div onClick={() => findChatMessages(element._id)} key={element._id}>
+              <div
+                onClick={() => findChatMessages(element._id, element.name)}
+                key={element._id}
+              >
                 <img
                   src={`http://localhost:8000/images/${element.profileImage}`}
                   alt=""
@@ -129,24 +135,41 @@ const Chat = () => {
               </div>
             );
           })}
+        {users.length === 0 && <p>Currently no friends</p>}
       </aside>
       <div className={classes.chat}>
-        <div className={classes.chatMessages}>
+        <div className={classes.chatMessages} ref={messagesEndRef}>
           {message.length > 0 &&
             message.map(element => (
-              <h1
-                key={Math.random()}
-                className={
-                  element.author == localStorage.getItem("id") ? classes.me : null
-                }
-              >
-                {element.text}
-              </h1>
+              <>
+                <h1
+                  key={Math.random()}
+                  className={
+                    element.author == localStorage.getItem("id") ? classes.me : null
+                  }
+                >
+                  {element.text}
+                </h1>
+
+                <h2
+                  className={
+                    element.author == localStorage.getItem("id") ? classes.mine : null
+                  }
+                >
+                  {new Date(Date.parse(element.created)).toLocaleString()}
+                </h2>
+              </>
             ))}
         </div>
         <div className={classes.chatForm}>
-          <textarea className={classes.formInput} ref={messageInput}></textarea>
-          <button onClick={sentMessage}>Send</button>
+          <textarea
+            placeholder="Say something ..."
+            className={classes.formInput}
+            ref={messageInput}
+          ></textarea>
+          <button onClick={currentUser ? sentMessage : null}>
+            <AiOutlineSend className={classes.sendIcon}></AiOutlineSend>
+          </button>
         </div>
       </div>
     </div>
