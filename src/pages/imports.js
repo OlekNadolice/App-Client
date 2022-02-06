45c6e5b28a4161771d93207d5f033eab576f7188
Aleksander Cygan <aleksandercygan@MacBook-Air-Aleksander.local>
2022-02-06 21:04:02 +0100

    Added one file for exporting all pages

---
+++ src/pages/imports.js
@@ -0,0 +1,11 @@
+export { Chat } from "./Chat/Chat";
+export { Welcome } from "./Welcome/Welcome";
+export { Login } from "./Login/Login";
+export { Register } from "./Register/Register";
+export { Settings } from "./Settings/Settings";
+export { User } from "./User/User";
+export { Home } from "./Home/Home";
+export { Info } from "./Info/Info";
+export { Friends } from "./Friends/Friends";
+export { NotFounds } from "./NotFound/NotFounds";
+export { OAuthVerificationPage } from "./oAuthVerification/OAuthVerificationPage";
