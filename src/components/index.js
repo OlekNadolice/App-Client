d95e36f3fb632e67540a0ae196f6b4fabe37533a
Aleksander Cygan <aleksandercygan@MacBook-Air-Aleksander.local>
2022-02-06 21:04:24 +0100

    Added one file for exporting all components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { Comment } from "./Comment/Comment";
+export { Footer } from "./Footer/Footer";
+export { FriendsRequest } from "./FriendsRequest/FriendsRequest";
+export { Header } from "./Header/Header";
+export { Modal } from "./Modal/Modal";
+export { Question } from "./Question/Question";
+export { UserCard } from "./UserCard/UserCard";
